2efbb3366a921307169906cbbef680c2cf020039
Bryan Vidal <bryan.vidal@hetic.net>
2016-05-12 16:08:12 +0200

    intégration page single + google map

--- magentoHetic/assets/js/app.js
+++ magentoHetic/assets/js/app.js
@@ -1,35 +1,50 @@
 function initMap() {
 
+    var myLatLng = {lat: -25.363, lng: 131.044};
+    var myLatLng2 = {lat: -30.363, lng: 131.044};
   // Specify features and elements to define styles.
-  var styleArray = [
-    {
-      featureType: "all",
-      stylers: [
-       { saturation: -80 }
-      ]
-    },{
-      featureType: "road.arterial",
-      elementType: "geometry",
-      stylers: [
-        { hue: "#00ffee" },
-        { saturation: 50 }
-      ]
-    },{
-      featureType: "poi.business",
-      elementType: "labels",
-      stylers: [
-        { visibility: "off" }
-      ]
-    }
-  ];
+  var styleArray = 
+      [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+  ;
+
 
   // Create a map object and specify the DOM element for display.
+
+  var iconBase = {
+    url: "http://jeromevanetty.com/pointer.svg", // url
+    scaledSize: new google.maps.Size(50, 63), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+};
+
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 48.856614, lng: 2.3522219000000177},
     scrollwheel: false,
     // Apply the map style array to the map.
     styles: styleArray,
     zoom: 8
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: 48.856614, lng: 2.3522219000000177},
+    icon: iconBase,
+  });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: 48.956614, lng: 2.252221900000017},
+    icon: iconBase,
+  });
+  var marker = new google.maps.Marker({
+    map: map,
+    icon: iconBase,
+    position: {lat: 48.556614, lng: 2.552221900000017},
+  });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: 48.256614, lng: 2.222221900000017},
+    icon: iconBase,
   });
 }
 
---
+++ magentoHetic/single.php
@@ -0,0 +1,52 @@
+<?php include 'tpl/header.php' ?>
+<body class="page-archive">
+    <header>
+        <div class="logo">
+            <img src="assets/images/logo.png" alt="" />
+        </div>
+        <div class="links">
+            <div class="point-partners">
+                <img src="assets/images/ico-place.png" alt="" />
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row content-page">
+            <div class="col-sm-12">
+                <div class="title-page">
+                    Catégories
+                </div>
+                <div class="description-page">
+                    Séléctionnez ou recherchez votre activité.
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <div class="article">
+                    <div class="img-article big">
+                        <img src="https://static.pexels.com/photos/33443/pexels-photo.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-7">
+                <div class="description-single">
+                    <h3>Journée découverte avec guide</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <ul class="info">
+                        <li>Niveau : Confirmé</li>
+                        <li>Durée : 5h</li>
+                    </ul>
+
+                    <div class="price-single">
+                        <div class="price-product">
+                            prix : 240$
+                        </div>
+                        <a href="#" class="addtocart">Ajouter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="map">
+
+    </div>
+<?php include 'tpl/footer.php' ?>
